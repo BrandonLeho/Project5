f65f59038e8262926fdf73fd8bd116e0c588cabb
BrandonLeho <89223038+BrandonLeho@users.noreply.github.com>
2021-11-29 13:53:22 -0800

    Add files via upload

--- GraphTraversalDriver.java
+++ GraphTraversalDriver.java
@@ -40,8 +40,9 @@ public class GraphTraversalDriver
 		System.out.print("Depth-First Traversal order: ");
 		depthFirst(0, graph);
 		
-		System.out.println();
+		System.out.println("\n");
 		
+		//output for Adjacency Matrix
 		boolean[][] adjacent = new boolean[length][length];
 		
 		for(int i = 0; i < length; i++)
@@ -60,22 +61,36 @@ public class GraphTraversalDriver
 			}
 			System.out.println();
 		}
-	}
+		
+		System.out.println();
+		
+		//output for Adjacency List
+		for(int i = 0; i < length; i++)
+		{
+			System.out.print("Adjacency List for vertex " + graph.getLabel(i) + ":\nhead");
+			
+			for(int j = 0; j < length; j++)
+				if(graph.isEdge(i, j))
+					System.out.print(" -> " + graph.getLabel(j));
+			
+			System.out.println("\n");
+		}
+	} //end main
 	
 	public static void breadthFirst(int origin, Graph<String> graph)
 	{	
-		Bag<Integer> visited = new Bag<>();
+		Bag<Integer> visited = new Bag<>();	//Creates new bag to store visited vertices
 		
 		visited.add(origin);
 		System.out.print(graph.getLabel(origin));
 		
-		int[] queue = new int[10];
-		int queueCount = 0;
-		boolean countdown = false;
+		int[] queue = new int[10];	//Array to hold vertices that have not been the front vertex yet
+		int queueCount = 0;			//Counts queue position
+		boolean countdown = false;	//Determine whether to shorten the queue or not
 		
-		int[] neighbors = graph.neighbors(origin);		
+		int[] neighbors = graph.neighbors(origin);	//Assigns vertex's neighbors to neighbors array	
 		
-		for(int i = 0; i < neighbors.length; i++)
+		for(int i = 0; i < neighbors.length; i++)	//Add origin's neighbors to queue and visited
 		{
 			queue[i] = neighbors[i];
 			queueCount++;
@@ -83,11 +98,11 @@ public class GraphTraversalDriver
 			System.out.print(graph.getLabel(neighbors[i]));
 		}
 		
-		while(queueCount != 0)
+		while(queueCount != 0) //while queue isn't empty
 		{
 			neighbors = graph.neighbors(queue[0]);
 			
-			for(int i = 0; i < neighbors.length; i++)
+			for(int i = 0; i < neighbors.length; i++)	//visit the origin's neighbor's neighbors and count these as visited
 			{
 				if(!visited.contains(neighbors[i]))
 				{
@@ -99,7 +114,7 @@ public class GraphTraversalDriver
 				}
 			}
 			
-			for(int i = 0; i < queue.length - 1; i++)	
+			for(int i = 0; i < queue.length - 1; i++)	//shorten queue by one
 				queue[i] = queue[i + 1];		
 
 			if(countdown == true)
@@ -109,15 +124,15 @@ public class GraphTraversalDriver
 	
 	public static void depthFirst(int origin, Graph<String> graph) 
 	{
-        Stack<Integer> stack = new Stack<>();
-        Bag<Integer> visited = new Bag<>();
+        Stack<Integer> stack = new Stack<>();	//Stack vertices to visit neighbors
+        Bag<Integer> visited = new Bag<>();		//Bag to hold visited vertices
         
         stack.push(origin);
         System.out.print(graph.getLabel(origin));
         
-        int[] neighbors = graph.neighbors(stack.peek());
+        int[] neighbors = graph.neighbors(stack.peek()); //Assigns origin's neighbors to neighbors array
         
-        if(!visited.contains(neighbors[0]))
+        if(!visited.contains(neighbors[0]))	//Add origin's neighbors to top of the stack and visited
         {
         	System.out.print(graph.getLabel(neighbors[0]));
         	visited.add(neighbors[0]);
@@ -127,20 +142,20 @@ public class GraphTraversalDriver
         neighbors = graph.neighbors(neighbors[0]);
         
         int breakLoop = 0;    
-        while(breakLoop != 2)
+        while(breakLoop != 2) 
         {  
-            if(!visited.contains(neighbors[0]))
+            if(!visited.contains(neighbors[0])) //Visit the nighbor's neighbors and so on.
             {
             	System.out.print(graph.getLabel(neighbors[0]));
             	visited.add(neighbors[0]);
             	stack.push(neighbors[0]);
             	neighbors = graph.neighbors(neighbors[0]);
             }
-            else if(!stack.isEmpty())
+            else if(!stack.isEmpty())	//If vertex has no neighbors, backtrack one vertex and search for any missed neighbors
             { 	
             	neighbors = graph.neighbors(stack.pop());
             	
-            	for(int j = 0; j < neighbors.length - 1; j++)
+            	for(int j = 0; j < neighbors.length - 1; j++) //look for missed neighbors by canceling out the first neighbor option
     				neighbors[j] = neighbors[j + 1];			
             	
             	if(stack.isEmpty())
