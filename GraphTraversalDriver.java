edaf147afa94cb24ec4a3ba8dd7624d358368cf3
BrandonLeho <89223038+BrandonLeho@users.noreply.github.com>
2021-11-28 22:19:11 -0800

    Add files via upload

---
+++ GraphTraversalDriver.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class GraphTraversalDriver 
+{
+
+	public static void main(String args[])
+	{
+		int length = 9;
+	
+		Graph<String> graph = new Graph<>(length);
+		
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 3);
+		graph.addEdge(0, 4);
+		graph.addEdge(1, 4);
+		graph.addEdge(2, 1);
+		graph.addEdge(3, 6);
+		graph.addEdge(4, 5);
+		graph.addEdge(4, 7);
+		graph.addEdge(5, 7);
+		graph.addEdge(5, 2);
+		graph.addEdge(6, 7);
+		graph.addEdge(7, 8);
+		graph.addEdge(8, 5);
+		
+		graph.setLabel(0, "A");
+		graph.setLabel(1, "B");
+		graph.setLabel(2, "C");
+		graph.setLabel(3, "D");
+		graph.setLabel(4, "E");
+		graph.setLabel(5, "F");
+		graph.setLabel(6, "G");
+		graph.setLabel(7, "H");
+		graph.setLabel(8, "I");
+		
+		
+		breadthFirst(0, graph);
+	}
+	
+	public static void breadthFirst(int origin, Graph<String> graph)
+	{	
+		Bag<Integer> visited = new Bag<>();
+		visited.add(origin);
+
+		System.out.print(graph.getLabel(origin));
+		
+		int[] queue = new int[10];
+		int[] neighbors;
+		int queueCount = 0;
+		boolean countdown = false;
+		
+		neighbors = graph.neighbors(origin);		
+		
+		for(int i = 0; i < neighbors.length; i++)
+		{
+			queue[i] = neighbors[i];
+			queueCount++;
+			visited.add(neighbors[i]);
+			System.out.print(graph.getLabel(neighbors[i]));
+		}
+		
+		for(int j = 0; j < queue.length; j++)
+		{
+			neighbors = graph.neighbors(queue[0]);
+			
+			for(int i = 0; i < neighbors.length; i++)
+			{
+				if(!visited.contains(neighbors[i]))
+				{
+					queue[queueCount] = neighbors[i];
+					queueCount++;
+					countdown = true;
+					visited.add(neighbors[i]);
+					System.out.print(graph.getLabel(neighbors[i]));		
+				}
+			}
+			
+			for(int i = 0; i < queue.length - 1; i++)
+			{
+				queue[i] = queue[i + 1];
+				
+			}
+			if(countdown == true)
+				queueCount--;
+		}
+	} //end BreadthFirst
+}
